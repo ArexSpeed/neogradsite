302f3e95af2bac9e5bf32425d38c9dfcff57d1eb
Arek Cichocki <arexspeed95@gmail.com>
2021-02-15 18:02:22 +0100

    city - mobile and menu

--- src/examples/City.jsx
+++ src/examples/City.jsx
@@ -1,40 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 import './City.css';
 
 const City = () => {
+  const [hide, setHide] = useState(false)
   return (
     <section className="hero-city">
-        <nav className="navbar-city">
-              <div className="nav-logo">CityLook</div>
-                <ul className="city-menu">
-                  <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkgreen">Best places</button>
-                    <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkblue">Theaters</button>
-                    <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkgold">Restaurants</button>
-                    <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darksilver">Sightseeing</button>
-                    <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkviolet">Tickets</button>
-                    <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkred">Travel</button>
-                </ul>
-             <div className="nav-link">
-               <Link to='/'>
-                 NeoGrad
-               </Link>
-              </div>
-        </nav>
-        <main className='main-city'>
-            <h1 className="city-title">
-                Discover your city once again!
-            </h1>
-            <h4 className="city-subtitle">
-              Find the best places, restaurant and more in our beautiful city!
-            </h4>
-            <div className="city-button">
-            <button class="btn btn-rect-5 btn-round-5 btn-pulse-fast color-neonblue">Free Ticket</button>
-            </div>
-        </main>
+      <nav className="navbar-city-top">
+        <div class="hamburger-menu" onClick={() => setHide(!hide)}>
+          <span class="bar"></span>
+        </div>
+        <div className="nav-logo">CityLook</div>
+      </nav>
+      <nav className={`navbar-city ${hide ? "hide" : ""}`}>
+        <ul className="city-menu">
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkgreen">
+            Best places
+          </button>
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkblue">
+            Theaters
+          </button>
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkgold">
+            Restaurants
+          </button>
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darksilver">
+            Sightseeing
+          </button>
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkviolet">
+            Tickets
+          </button>
+          <button class="btn btn-rect-long-6 btn-round-5-right btn-slide-left color-darkred">
+            Travel
+          </button>
+        </ul>
+        <div className="nav-link">
+          <Link to="/">NeoGrad</Link>
+        </div>
+      </nav>
+      <main className="main-city">
+        <h1 className="city-title">Discover your city once again!</h1>
+        <h4 className="city-subtitle">
+          Find the best places, restaurant and more in our beautiful city!
+        </h4>
+        <div className="city-button">
+          <button class="btn btn-rect-5 btn-round-5 btn-pulse-fast color-neonblue">
+            Free Ticket
+          </button>
+        </div>
+      </main>
     </section>
-  )
+  );
 }
 
 export default City
