4da2bc17cfcce849ea31951cf7435ad39070abce
Arek Cichocki <arexspeed95@gmail.com>
2021-02-16 23:00:30 +0100

    icons & names

--- src/examples/Relax/Relax.js
+++ src/examples/Relax/Relax.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
+import AirlineSeatIndividualSuiteIcon from '@material-ui/icons/AirlineSeatIndividualSuite';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
+import BackupIcon from '@material-ui/icons/Backup';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 
 import './Relax.css';
 
@@ -7,6 +15,10 @@ const Relax = () => {
     <div className="relax-container">
       <div className="relax-circle"></div>
       <div className="relax-line"></div>
+      <div className="relax-info">
+        What Yoga and meditation can help you?
+      </div>
+
       <div className="relax-textBg">
       <div className="relax-text relax"><h2>RE
         <div>L</div><div>AX</div></h2></div>
@@ -14,14 +26,38 @@ const Relax = () => {
         <div>C</div><div>US</div></h2></div>
       </div>
         <div className="menu-relax">
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-violet">1</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-neonsilver">2</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-darksilver">3</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-silver">4</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-violet">5</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-neonpink">6</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-pink">7</div>
-          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-lightpink">8</div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-violet">
+            <AccessibilityNewIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">BODY</p>
+          </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-neonsilver">
+            <AirlineSeatIndividualSuiteIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">SLEEP</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-darksilver">
+            <AlarmIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">TIME</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-silver">
+            <AllInclusiveIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">MIND</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-violet">
+            <AutorenewIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">RECOVER</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-neonpink">
+            <BackupIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">SOUL</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-pink">
+            <CallSplitIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">RELATION</p>
+            </div>
+          <div className="btn-relax btn btn-circle-3 btn-shadow-behind color-lightpink">
+            <AttachMoneyIcon style={{ fontSize: 40 }} />
+            <p className="btn-text">MONEY</p>
+            </div>
         </div>
     </div>
   )
