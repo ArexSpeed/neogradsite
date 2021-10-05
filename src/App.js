5d03a5ea91888800f6392033ec895b3973281d3c
Arek Cichocki <arexspeed95@gmail.com>
2021-10-05 23:13:53 +0200

    remove old files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './App.css';
-import Build from './pages/Build';
-import Docs from './pages/Docs';
-import Main from './pages/Main';
-import Nav from './pages/Nav';
-import City from './examples/City/City';
-import Budget from './examples/Budget/Budget';
-import Relax from './examples/Relax/Relax';
-import AISite from './examples/AISite/AISite';
 import Hero from './components/Hero';
 import Builder from './components/Builder';
 
